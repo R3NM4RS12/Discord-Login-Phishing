d6eacb644ae4352d2697ab06b0c473b90b08fbbe
R3NM4RS12 <129458396+R3NM4RS12@users.noreply.github.com>
2023-05-04 12:35:36 -0400

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAFRUo80abuYQTFLfH4lmqrNvkvKu3ME2c",
+  authDomain: "account-a104c.firebaseapp.com",
+  projectId: "account-a104c",
+  storageBucket: "account-a104c.appspot.com",
+  messagingSenderId: "446517167274",
+  appId: "1:446517167274:web:b97efb6b8d124ae6f27cd3",
+  measurementId: "G-EQ7GXFTL7S"
 };
 
 firebase.initializeApp(firebaseConfig);
